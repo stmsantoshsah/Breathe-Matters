f1a73eb85c7158830ba474bec76e3a29d358874e
santosh <yousilenceplease@gmail.com>
2025-03-01 19:55:25 +0530

    Responsive Issue Fixed

--- src/components/sections/CapnoTrainer.tsx
+++ src/components/sections/CapnoTrainer.tsx
@@ -26,10 +26,10 @@ const CapnoTrainer: React.FC = () => {
             <div className="container">
                 <div className='flex justify-between md:flex-nowrap flex-wrap gap-5'>
                     <div className='xl:max-w-[540px] lg:max-w-[400px] md:max-w-[350px]'>
-                        <h2 className='text-primary font-bold sm:!font-[50px] md:!font-[40px] !font-[30px] glining'>{CapnoTrainerData.title}</h2>
+                        <h2 className='text-primary font-bold sm:!font-[50px] md:!font-[40px] !font-[30px] glining relative'>{CapnoTrainerData.title}</h2>
                         <p className=''>{CapnoTrainerData.description}</p>
                     </div>
-                    <div className='md:!mt-0 !mt-6' >
+                    <div className='md:!mt-0 !mt-6 capnotrainer_right relative ' >
                         {CapnoTrainerData.points.map((item, i) => (
                             <div key={i} className='bg-[#E6F2F2] rounded-[8px] !p-[10px] !mb-[22px] w-auto flex gap-2'>
                                 <img src={item.icons} alt="" />
